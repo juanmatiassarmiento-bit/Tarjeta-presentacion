4836675834f476ab2aaf43917d9c0599ed2ba48a
juanmatiassarmiento-bin <juanmatiassarmiento@gmail.com>
2026-05-07 09:16:41 -0300

    Agregar onPressIn, onPressOut, onLongPress, hitSlop y estado pressed

--- components/Presentacion.js
+++ components/Presentacion.js
@@ -1,7 +1,10 @@
-import { StyleSheet, Text, Image, Linking, ScrollView, View } from 'react-native';
+import { StyleSheet, Text, Image, ScrollView, View, Linking, useState } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 
+
 export default function Presentacion() {
+  const [pressed, setPressed] = useState(false);
+
   const abrirInstagram = () => {
     Linking.openURL(
       'https://www.instagram.com/juanma_tias?igsh=MTRlYmJ5aHdkbmZpeQ=='
@@ -14,14 +17,22 @@ export default function Presentacion() {
         <Image source={require('./assets/imagen.jpg')} style={styles.imagen} />
 
         <View style={styles.infoContainer}>
-  <Text style={styles.texto}>Nombre: Juan Matías Sarmiento</Text>
-  <Text style={styles.texto}>Edad: 17 años</Text>
-  <Text style={styles.texto}>Fecha de nacimiento: 14-04-2009</Text>
+          <Text style={styles.texto}>Nombre: Juan Matías Sarmiento</Text>
+          <Text style={styles.texto}>Edad: 17 años</Text>
+          <Text style={styles.texto}>Fecha de nacimiento: 14-04-2009</Text>
 
-  <Text style={styles.link} onPress={abrirInstagram}>
-    Ir a mi Instagram
-  </Text>
-</View>
+          <Text
+            style={[styles.link, pressed && styles.linkPresionado]}
+            onPress={abrirInstagram}
+            onPressIn={() => setPressed(true)}
+            onPressOut={() => setPressed(false)}
+            onLongPress={() => alert('¡Mantuviste presionado!')}
+             delayLongPress={800}
+            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+          >
+            Ir a mi Instagram
+          </Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -29,37 +40,32 @@ export default function Presentacion() {
 
 const styles = StyleSheet.create({
   infoContainer: {
-  alignItems: 'center',
-  width: '100%',
-},
-
+    alignItems: 'center',
+    width: '100%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#f2f2f2',
   },
-
   scroll: {
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 40,
   },
-
   imagen: {
     width: 140,
     height: 140,
     borderRadius: 70,
     marginBottom: 20,
   },
-
   texto: {
     fontSize: 20,
     marginVertical: 6,
-    color: '#000', // 👈 negro para que SIEMPRE se vea
+    color: '#000',
     fontWeight: 'bold',
     textAlign: 'center',
   },
-
   link: {
     marginTop: 25,
     color: '#fff',
@@ -69,5 +75,9 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 25,
+  },
+  linkPresionado: {
+    backgroundColor: '#a0204a',
+    transform: [{ scale: 0.96 }],
   },
 });
